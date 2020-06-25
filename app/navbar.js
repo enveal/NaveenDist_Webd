472edad96cde8162fbda9c0470be2021826ea2c9
enveal <envealsood@gmail.com>
2020-06-25 22:47:35 +0530

    Minor bugs fixed

--- app/navbar.js
+++ app/navbar.js
@@ -1,7 +1,7 @@
 let navlinks = $('.navbar-nav .nav-link')
 console.log(navlinks)
 navlinks.click((event) => {
-    let componentUrl = `../components/${$(event.target).attr('data-component')}.html`
+    let componentUrl = `./components/${$(event.target).attr('data-component')}.html`
     console.log(componentUrl)
     $('#content').load(componentUrl)
 })
